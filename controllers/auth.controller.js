d0e788b0a9eab11339287cca4d121fb1aa70b0b7
SHair <sonhai.it.2048@gmail.com>
2025-06-06 17:49:09 +0700

    Create api for login:

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { generateUniqueUsername } = require("../utils/username.js");
 const Account = require("../models/account.model.js");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -32,4 +33,46 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  console.log("Login request received:", req.body);
+  const { email, password } = req.body;
+
+  // console.log("Login request received:", email, password);
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Email and password are required." });
+  }
+
+  try {
+    const account = await Account.findOne({
+      email,
+    });
+    console.log("Account found:", account);
+    const uid = account._id;
+
+    if (!account) {
+      return res.status(404).json({ message: "Account not found." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, account.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid password." });
+    }
+
+    const token = jwt.sign({ id: uid }, "SECRET_KEY", { expiresIn: "1d" });
+
+    res.status(200).json({
+      message: "Login successful.",
+      token,
+      account: { email: account.email, name: account.name },
+    });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+};
+
+module.exports = { registerUser, loginUser };
