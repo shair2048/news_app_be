6a72204ddce6622a7a0750c3014ea016e2acd75a
SHair <sonhai.it.2048@gmail.com>
2025-07-18 07:40:54 +0700

    Removed old API (in comment)

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -9,36 +9,6 @@ export const signUp = async (req, res, next) => {
   const session = await mongoose.startSession();
   session.startTransaction();
 
-  // const { name, email, password } = req.body;
-
-  // if (!name || !email || !password)
-  //   return res.status(400).json({
-  //     message: "Name, email and password are required.",
-  //   });
-
-  // try {
-  //   const username = await generateUniqueUsername(name);
-
-  //   const saltRounds = 10;
-  //   const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-  //   const account = new Account({
-  //     name,
-  //     username,
-  //     email: email.toLowerCase(),
-  //     password: hashedPassword,
-  //   });
-
-  //   await account.save();
-  //   res.status(201).json({
-  //     message: "Account created successfully.",
-  //   });
-  // } catch {
-  //   res.status(500).json({
-  //     message: "Internal server error.",
-  //   });
-  // }
-
   try {
     const { name, email, password } = req.body;
 
@@ -92,51 +62,6 @@ export const signUp = async (req, res, next) => {
 };
 
 export const signIn = async (req, res, next) => {
-  // const { email, password } = req.body;
-
-  // // console.log("Login request received:", email, password);
-  // if (!email || !password)
-  //   return res.status(400).json({
-  //     message: "Email and password are required.",
-  //   });
-
-  // try {
-  //   const account = await Account.findOne({
-  //     email,
-  //   });
-
-  //   const uid = account._id;
-
-  //   if (!account)
-  //     return res.status(404).json({
-  //       message: "Account not found.",
-  //     });
-
-  //   const isPasswordValid = await bcrypt.compare(password, account.password);
-
-  //   if (!isPasswordValid)
-  //     return res.status(401).json({
-  //       message: "Invalid password.",
-  //     });
-
-  //   const token = jwt.sign({ id: uid }, "SECRET_KEY", {
-  //     expiresIn: "1d",
-  //   });
-
-  //   res.status(200).json({
-  //     message: "Login successful.",
-  //     token,
-  //     account: {
-  //       email: account.email,
-  //       name: account.name,
-  //     },
-  //   });
-  // } catch {
-  //   res.status(500).json({
-  //     message: "Internal server error.",
-  //   });
-  // }
-
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
