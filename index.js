974e4c1c6a90a3a899962905536353f4957515ef
SHair <sonhai.it.2048@gmail.com>
2025-11-29 00:33:40 +0700

    Implemented a cron job to crawl RSS feeds every 30 minutes.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import { PORT } from "./config/env.js";
 import connectDatabase from "./src/database/mongodb.js";
 import app from "./src/app.js";
+import { startRssCronJob } from "./src/services/cronRss.service.js";
 
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 
   await connectDatabase();
 });
+
+startRssCronJob();
--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 import {
   getAllArticles,
   // getArticleById,
-  fetchArticlesData,
+  // fetchArticlesData,
 } from "../controllers/article.controllers.js";
 
 const articleRoute = Router();
@@ -14,6 +14,6 @@ const articleRoute = Router();
 // articleRoute.post("/", upload.single("imageUrl"), createArticle);
 articleRoute.get("/", getAllArticles);
 // articleRoute.get("/:id", getArticleById);
-articleRoute.get("/fetch", fetchArticlesData);
+// articleRoute.get("/fetch", fetchArticlesData);
 
 export default articleRoute;
