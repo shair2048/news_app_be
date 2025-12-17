6d269c27392ab33103eee702199e5604be9040ef
SHair <sonhai.it.2048@gmail.com>
2025-12-18 02:55:16 +0700

    Route to get article by id

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import {
   getAllArticles,
+  getArticleById,
   fetchArticlesData,
 } from "../controllers/article.controllers.js";
 
 const articleRoute = Router();
 
 articleRoute.get("/", getAllArticles);
-// articleRoute.get("/:id", getArticleById);
+articleRoute.get("/:id", getArticleById);
 articleRoute.get("/fetch/all", fetchArticlesData);
 
 export default articleRoute;
