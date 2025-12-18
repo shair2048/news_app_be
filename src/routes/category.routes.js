ccebeb09fcab5130902d8fd33c365d17e7f5996e
SHair <sonhai.it.2048@gmail.com>
2025-12-19 02:28:28 +0700

    Create Article preview API to response article previews data with 4 items per category

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getAllCategories,
   getArticlesByCategory,
+  getArticlesPreviewAndCateory,
 } from "../controllers/category.controllers.js";
 
 const categoryRoute = Router();
 
 categoryRoute.get("/", getAllCategories);
+categoryRoute.get("/articles/preview", getArticlesPreviewAndCateory);
 categoryRoute.get("/:slug/articles", getArticlesByCategory);
 
 export default categoryRoute;
