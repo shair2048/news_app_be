5d997cbebbb8051193de897d690df6a9f3e86db2
SHair <sonhai.it.2048@gmail.com>
2025-12-27 21:07:32 +0700

    Created toggle bookmark and get bookmark articles api

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -2,16 +2,21 @@ import { Router } from "express";
 import {
   getAllArticles,
   getArticleById,
+  toggleBookmark,
   fetchArticlesData,
+  getBookmarkedArticles,
   getLatestArticles,
   summarizeArticle,
 } from "../controllers/article.controllers.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const articleRoute = Router();
 
 articleRoute.get("/", getAllArticles);
 articleRoute.get("/latest", getLatestArticles);
+articleRoute.get("/bookmarked", authorize, getBookmarkedArticles);
 articleRoute.get("/fetch/all", fetchArticlesData);
+articleRoute.post("/bookmark/:id", authorize, toggleBookmark);
 articleRoute.get("/:id", getArticleById);
 articleRoute.post("/:id/summarize", summarizeArticle);
 
