8acb980ed3b0d2834c172d420306f7153429cea2
SHair <sonhai.it.2048@gmail.com>
2026-01-10 00:53:56 +0700

    API for authentication of liveblocks has been changed to allow read access for guest

--- src/controllers/liveblocks.controllers.js
+++ src/controllers/liveblocks.controllers.js
@@ -11,24 +11,34 @@ export const authenticateLiveblocks = async (req, res) => {
 
   const { room } = req.body;
 
-  console.log("Liveblocks Auth User:", user);
+  let session;
 
-  if (!user) {
-    return res.status(403).json({ message: "User not identified" });
-  }
+  if (user) {
+    session = liveblocks.prepareSession(user._id.toString(), {
+      userInfo: {
+        name: user.name || "Anonymous",
+        avatar: user.avatar,
+        role: user.role,
+        isGuest: false,
+      },
+    });
 
-  // Identifying Users for Liveblocks (Prepare Session)
-  const session = liveblocks.prepareSession(user._id.toString(), {
-    userInfo: {
-      name: user.name || "Anonymous",
-      avatar: user.avatar,
-      role: user.role,
-    },
-  });
-
-  // Authorization for Room
-  if (room) {
-    session.allow(room, session.FULL_ACCESS);
+    // Authorization for Room
+    if (room) {
+      session.allow(room, session.FULL_ACCESS);
+    }
+  } else {
+    const guestId = `guest-${Math.floor(Math.random() * 10000)}`;
+    session = liveblocks.prepareSession(guestId, {
+      userInfo: {
+        name: "Guest",
+        avatar: "",
+        isGuest: true,
+      },
+    });
+    if (room) {
+      session.allow(room, session.READ_ACCESS);
+    }
   }
 
   const { body, status } = await session.authorize();
@@ -45,15 +55,31 @@ export const resolveUsers = async (req, res) => {
 
     const users = await User.find(
       { _id: { $in: userIds } },
-      { name: 1, email: 1, role: 1, avatar: 1 }
+      { name: 1, role: 1, avatar: 1 }
     );
 
-    const userMap = users.map((user) => ({
-      name: user.name,
-      avatar: user.avatar,
-    }));
+    const userMap = users.reduce((acc, user) => {
+      acc[user._id.toString()] = user;
+      return acc;
+    }, {});
 
-    return res.status(200).json(userMap);
+    const orderedUsers = userIds.map((id) => {
+      const user = userMap[id];
+
+      if (user) {
+        return {
+          name: user.name,
+          avatar: user.avatar,
+        };
+      }
+
+      return {
+        name: "Deleted User",
+        avatar: "",
+      };
+    });
+
+    return res.status(200).json(orderedUsers);
   } catch (error) {
     console.error("Resolve Users Error:", error);
     return res.status(500).json({ message: "Failed to resolve users" });
